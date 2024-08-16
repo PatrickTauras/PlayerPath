33ed2fe602759aafb9e4fbf1c7ca89331542100d
Patrick Tauras <ptaura2@illinois.edu>
2024-08-15 19:33:27 -0500

    Updates

--- src/Components/NavBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-
-export const NavBar = () => {
-    const location = useLocation();
-
-    // Render nothing if not on the root page
-    return null;
-};
